56b8f807b741fda4443da3961e10a394c23b540d
hellen <matheus.camargo.maciel@escola.pr.gov.br>
2025-05-08 12:27:36 -0300

    arquivo exemplo 3

--- pythonProject/capitulo 2/exemplo2nomesdevariaveis.py
+++ pythonProject/capitulo 2/exemplo2nomesdevariaveis.py
@@ -1,3 +1,6 @@
-#não pode começar com numero
-#nem caracteres especiais
-#não pode utilizar palavras chaves reservadas
+# não pode começar com numero
+# nem caracteres especiais
+# não pode utilizar palavras chaves reservadas
+
+# sem caracteres especiais
+# more@ = 1000
