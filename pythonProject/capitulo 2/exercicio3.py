84715fc18063027f165b507b85fe8b3df9c4a10e
pablo <pablo.fesqueiras@escola.pr.gov.br>
2025-05-16 12:26:03 -0300

    exercicio 3

--- pythonProject/capitulo 2/exercicio3.py
+++ pythonProject/capitulo 2/exercicio3.py
@@ -1,19 +1,40 @@
-#Quilometro 1 era 7:00 e 15 segundos, no quilometro
+print ("tempo do primeiro trecho")
+minutoPrimeiroTrecho = 8 * 60
+segundoPrimeiroTempo = 15
 
-print("tempo do primeiro trecho: 8min e 15seg)
-minutoPrimeiroTrecho = 8
-segundoPrimeiroTrecho = 15
-totalTempoPrimeiroTrecho = (8 * 60) + 15
-print("Tempo do primeiro trecho em segundos: ", totalTempoPrimeiroTrecho)
+#print(" Tempo do primeiro trecho em segundos: ", totalTempoPrimeiroTrecho)
 
-print("tempo do segundo trecho: 7min e 12seg")
-minutoSegundoTrecho = 7
-segundoSegundoTrecho = 12
-totalTempoSegundoTrecho = (7 * 60)) +12
-print("Tempo do segundo trecho em segundos: ", totalTempoSegundoTrecho)
+print("Tempo do segundo trecho: 7min e 12seg")
+minutoSegundoTrecho = (7 * 3) * 60
+segundoSegundotrecho = 12 * 3
+# print("Tempo do segundo trecho em segundos: ", totalTempoSegundoTrecho)
 
-print("Tempo do terceiro trecho: 9 min e 15 s")
-minutoTerceiroTrecho = 8
-segundoterceirotrecho = 15
-totalTempoTerceiroTrecho = (8 * 60) + 15
-print("Tempo do terceiro trecho em segundos: ", totalTempoTerceiroTrecho)
+print ("tempo do terceiro trecho: 8 min e 15 s")
+minutoTerceiroTrecho = 8 * 60
+segundoTerceiroTempo = 15
+#print(" Tempo do terceiro trecho em segundos: ", totalTempoTerceiroTrecho)
+# soma o total de minumetros e converte todos os minutos em segundos
+
+totalTempoTodosTrechosMinutos = (minutoPrimeiroTrecho + minutoSegundoTrecho + minutoTerceiroTrecho) / 60
+
+# soma valor total dos segundos
+totalTempoTodosTrechosSegundos = (segundoPrimeiroTempo + segundoSegundotrecho + segundoTerceiroTempo)
+
+restantesMinutos = int(totalTempoTodosTrechosSegundos / 60)
+restantesSegundos = totalTempoTodosTrechosSegundos % 60
+
+totalMinutos = totalTempoTodosTrechosMinutos + restantesMinutos
+totalSegundos = restantesSegundos
+
+print("soma de tempo de todos os trechos: ", totalMinutos, "Minutos")
+print("Soma de tempo de todos os trechos: ", totalSegundos, "segundos")
+
+horaInicialSegundos  = (6 * 60) * 60
+minutoInicialSegundos = 52 * 60
+horarioInicialSegundos = horaInicialSegundos + minutoInicialSegundos
+
+tempoTrechoMinutosSegundos = totalMinutos * 60
+
+horaChegada = int(((horarioInicialSegundos + tempoTrechoMinutosSegundos) / 60) / 60)
+minutoChegada = int(((minutoInicialSegundos + tempoTrechoMinutosSegundos) / 60) % 60)
+print(" O tempo de chegada foi de " , horaChegada, ":", minutoChegada, restantesSegundos)
