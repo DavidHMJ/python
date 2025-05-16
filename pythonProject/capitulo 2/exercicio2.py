a647de589443a0b0aea983dd4ecea859c97b4105
pablo <pablo.fesqueiras@escola.pr.gov.br>
2025-05-16 09:42:49 -0300

    exercicio3

---
+++ pythonProject/capitulo 2/exercicio2.py
@@ -0,0 +1,11 @@
+capa = 24.95
+desconto = capa * 0.40
+precolivraria = capa - desconto
+
+fretePrimeiroExemplar = 3.00
+freteRestanteExemplar = 0.75
+
+custoAtacadoPrimeiroExemplar = capa + fretePrimeiroExemplar
+custoAtacado = custoAtacadoPrimeiroExemplar + ((capa + freteRestanteExemplare) * 59)
+
+print('o custo total de atacado de 60 copias é de:', custoAtacado)
