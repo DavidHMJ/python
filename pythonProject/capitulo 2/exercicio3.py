36f8d10c26f7eea9a27af7df38335693d4b1f45e
pablo <pablo.fesqueiras@escola.pr.gov.br>
2025-05-16 09:55:44 -0300

    exercicio3

---
+++ pythonProject/capitulo 2/exercicio3.py
@@ -0,0 +1,19 @@
+#Quilometro 1 era 7:00 e 15 segundos, no quilometro
+
+print("tempo do primeiro trecho: 8min e 15seg)
+minutoPrimeiroTrecho = 8
+segundoPrimeiroTrecho = 15
+totalTempoPrimeiroTrecho = (8 * 60) + 15
+print("Tempo do primeiro trecho em segundos: ", totalTempoPrimeiroTrecho)
+
+print("tempo do segundo trecho: 7min e 12seg")
+minutoSegundoTrecho = 7
+segundoSegundoTrecho = 12
+totalTempoSegundoTrecho = (7 * 60)) +12
+print("Tempo do segundo trecho em segundos: ", totalTempoSegundoTrecho)
+
+print("Tempo do terceiro trecho: 9 min e 15 s")
+minutoTerceiroTrecho = 8
+segundoterceirotrecho = 15
+totalTempoTerceiroTrecho = (8 * 60) + 15
+print("Tempo do terceiro trecho em segundos: ", totalTempoTerceiroTrecho)
